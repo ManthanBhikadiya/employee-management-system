3c3aeba1b114f26ebb70a9486821d278bf33ba16
ManthanBhikadiya <manthanbhikadiya9931@gmail.com>
2026-08-27 17:26:34 +0530

    Auth Provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,31 @@
-import React, { useEffect, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import Login from './components/Auth/Login'
 import EmployeeDashboard from './components/Dashboard/EmployeeDashboard'
 import AdminDashboard from './components/Dashboard/AdminDashboard'
 import { getLocalStorage, setLocalStorage } from './utils/localStorage'
+import { AuthContext } from './context/AuthProvider'
 
 const App = () => {
 
   const [user, setUser] = useState(null)
+  const authData = useContext(AuthContext)
+
+  useEffect(() => {
+    if (authData) {
+      const loggedInUser = localStorage.getItem("loggedInUser")
+      if (loggedInUser) {
+        setUser(loggedInUser.role)
+      }
+    }
+  }, [authData])
 
   const handleLogin = (email, password) => {
     if (email == 'admin@me.com' && password == '123') {
       setUser('admin')
-      console.log(user);
-
-    } else if (email == 'user@me.com' && password == '123') {
+      localStorage.setItem('loggedInUser', JSON.stringify({ role: 'admin' }))
+    } else if (authData && authData.employees.find((e) => email == e.email && e.password == password)) {
       setUser('employee')
-      console.log(user);
+      localStorage.setItem('loggedInUser', JSON.stringify({ role: 'employee' }))
 
     }
     else {
@@ -24,10 +34,12 @@ const App = () => {
   }
 
 
+
+
   return (
     <>
       {!user ? <Login handleLogin={handleLogin} /> : ''}
-      {user == 'admin' ?<AdminDashboard/>: <EmployeeDashboard/>}
+      {user == 'admin' ? <AdminDashboard /> : <EmployeeDashboard />}
     </>
 
   )
--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -1,15 +1,15 @@
 import React, { useState } from 'react'
 
-const Login = ({handleLogin}) => {
+const Login = ({ handleLogin }) => {
 
-    const [email ,setEmail] = useState('')
-    const [password ,setPassword] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
 
 
     const submitHndler = (e) => {
         e.preventDefault()
-      
-        handleLogin(email,password)
+
+        handleLogin(email, password)
 
         setEmail('')
         setPassword('')
@@ -19,23 +19,23 @@ const Login = ({handleLogin}) => {
         <div className="flex h-screen w-screen items-center justify-center bg-[#1c1c1c]" >
             <div className="border-2 border-emerald-600 p-20 rounded-xl">
                 <form
-                onSubmit={(e)=>{
-                    submitHndler(e)
-                }}
-                 className='flex flex-col items-center justify-center'>
-                    <input
-                    value={email}
-                    onChange={(e)=>{
-                        setEmail(e.target.value)
+                    onSubmit={(e) => {
+                        submitHndler(e)
                     }}
-                    required className='text-black outline-none bg-transparent border-2 border-emerald-600 text-xl py-2 px-5 rounded-full' type="email" placeholder='Enter your email'
+                    className='flex flex-col items-center justify-center'>
+                    <input
+                        value={email}
+                        onChange={(e) => {
+                            setEmail(e.target.value)
+                        }}
+                        required className='text-black outline-none bg-transparent border-2 border-emerald-600 text-xl py-2 px-5 rounded-full' type="email" placeholder='Enter your email'
                     />
                     <input
-                    value={password}
-                    onChange={(e)=>{
-                        setPassword(e.target.value)
-                    }}
-                    required className='text-black outline-none bg-transparent border-2 border-emerald-600 text-xl py-2 px-5 rounded-full mt-4' type="password" placeholder='Enter your Password'
+                        value={password}
+                        onChange={(e) => {
+                            setPassword(e.target.value)
+                        }}
+                        required className='text-black outline-none bg-transparent border-2 border-emerald-600 text-xl py-2 px-5 rounded-full mt-4' type="password" placeholder='Enter your Password'
                     />
                     <button className='text-white border-none outline-none bg-emerald-600 hover:bg-emerald-700 cursor-pointer w-full text-xl py-3 px-5 rounded-full mt-10' >Login</button>
                 </form>
